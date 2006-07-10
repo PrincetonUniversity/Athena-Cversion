272485719e0f37488177899cb5a9a242c44b709a
jstone <jstone@aae0e298-1656-4984-9a04-95235c055e13>
2006-07-10 21:30:17 +0000

    Added x,y,z cordinates to dump, and time,dt to be compatible with IDL script.

--- src/dump_binary.c
+++ src/dump_binary.c
@@ -29,8 +29,8 @@ void dump_binary(Grid *pGrid, Output *pOut)
   int i, il = pGrid->is, iu = pGrid->ie;
   int j, jl = pGrid->js, ju = pGrid->je;
   int k, kl = pGrid->ks, ku = pGrid->ke;
-  float eos[2],*data;
-  Real *pq;
+  float dat[2],*datax,*datay,*dataz;
+  Real *pq,x1,x2,x3;
 
 #ifdef WRITE_GHOST_CELLS
   if(pGrid->Nx1 > 1){
@@ -70,37 +70,75 @@ void dump_binary(Grid *pGrid, Output *pOut)
 /* Write (gamma-1) and isothermal sound speed */
 
 #ifdef ISOTHERMAL
-  eos[0] = (float)0.0;
-  eos[1] = (float)Iso_csound;
+  dat[0] = (float)0.0;
+  dat[1] = (float)Iso_csound;
 #else
-  eos[0] = (float)Gamma_1 ;
-  eos[1] = (float)0.0;
+  dat[0] = (float)Gamma_1 ;
+  dat[1] = (float)0.0;
 #endif
-  fwrite(eos,sizeof(float),2,p_binfile);
+  fwrite(dat,sizeof(float),2,p_binfile);
+
+/* Write time, dt */
+
+  dat[0] = (float)pGrid->time;
+  dat[1] = (float)pGrid->dt;
+  fwrite(dat,sizeof(float),2,p_binfile);
 
 /* Allocate Memory */
 
-  if((data = (float *)malloc(ndata[0]*sizeof(float))) == NULL){
+  if((datax = (float *)malloc(ndata[0]*sizeof(float))) == NULL){
     ath_error("[dump_binary]: malloc failed for temporary array\n");
-    fclose(p_binfile);
+    return;
+  }
+  if((datay = (float *)malloc(ndata[1]*sizeof(float))) == NULL){
+    ath_error("[dump_binary]: malloc failed for temporary array\n");
+    return;
+  }
+  if((dataz = (float *)malloc(ndata[2]*sizeof(float))) == NULL){
+    ath_error("[dump_binary]: malloc failed for temporary array\n");
     return;
   }
 
-/* Write cell-centered date in Gas array pGrid->U[n] */
+/* compute x,y,z coordinates of cell centers, and write out */
+
+  for (i=il; i<=iu; i++) {
+    cc_pos(pGrid,i,jl,kl,&x1,&x2,&x3);
+    pq = ((Real *) &(x1));
+    datax[i-il] = (float)(*pq);
+  }
+  fwrite(datax,sizeof(float),(size_t)ndata[0],p_binfile);
+
+  for (j=jl; j<=ju; j++) {
+    cc_pos(pGrid,il,j,kl,&x1,&x2,&x3);
+    pq = ((Real *) &(x2));
+    datay[j-jl] = (float)(*pq);
+  }
+  fwrite(datay,sizeof(float),(size_t)ndata[1],p_binfile);
+
+  for (k=kl; k<=ku; k++) {
+    cc_pos(pGrid,il,jl,k,&x1,&x2,&x3);
+    pq = ((Real *) &(x3));
+    dataz[k-kl] = (float)(*pq);
+  }
+  fwrite(dataz,sizeof(float),(size_t)ndata[2],p_binfile);
+
+/* Write cell-centered data in Gas array pGrid->U[n] */
 
   for (n=0;n<NVAR; n++) {
     for (k=0; k<ndata[2]; k++) {
     for (j=0; j<ndata[1]; j++) {
       for (i=0; i<ndata[0]; i++) {
         pq = ((Real *) &(pGrid->U[k+kl][j+jl][i+il])) + n;
-        data[i] = (float)(*pq);
+        datax[i] = (float)(*pq);
       }
-      fwrite(data,sizeof(float),(size_t)ndata[0],p_binfile);
+      fwrite(datax,sizeof(float),(size_t)ndata[0],p_binfile);
     }}
   }
 
 /* close file and free memory */
   fclose(p_binfile); 
-  free(data); 
+  free(datax); 
+  free(datay); 
+  free(dataz); 
   free(fname);
 }
