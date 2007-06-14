806d47e9141f82fb2962407d68e6f2b7b8970936
jstone <jstone@aae0e298-1656-4984-9a04-95235c055e13>
2007-06-14 22:18:44 +0000

    Added periodic BCs.  Need to make work with MPI

--- src/selfg_fft.c
+++ src/selfg_fft.c
@@ -6,10 +6,13 @@
  *   1D, 2D and 3D using FFTs (actually, the 1D algorithm uses Forward 
  *   Elimination followed by Back Substitution: FEBS).
  *
- *   These routines require PERIODIC BCs and use the Jeans swindle.
+ *   These functions require PERIODIC BCs and use the Jeans swindle.
  *
- *   These routines use FFTW3.x, and for MPI parallel use Steve Plimpton's
- *   block decomposition routines added by N. Lemaster to /athena/fftsrc
+ *   The 2D and 3D f'ns use FFTW3.x, and for MPI parallel use Steve Plimpton's
+ *   block decomposition routines added by N. Lemaster to /athena/fftsrc.
+ *   This means to use these fns the code must be
+ *      (1) configured with --enable-fft
+ *      (2) compiled with links to FFTW libraries
  *
  * CONTAINS PUBLIC FUNCTIONS:
  *   selfg_by_fft_1d() - actually uses FEBS
@@ -28,9 +31,12 @@
 
 #include "ath_fft.h"
 
+/* plans for forward and backward FFTs; work space for FFTW */
 static struct ath_2d_fft_plan *fplan2d, *bplan2d;
 static struct ath_3d_fft_plan *fplan3d, *bplan3d;
 static ath_fft_data *work=NULL;
+
+#endif /* FFT_ENABLED */
 
 /*----------------------------------------------------------------------------*/
 /* selfg_by_fft_1d:  Actually uses forward elimination - back substituion!!
@@ -79,6 +85,7 @@ void selfg_by_fft_1d(Grid *pG, Domain *pD)
 #endif /* SELF_GRAVITY_USING_FFT */
 }
 
+#ifdef FFT_ENABLED
 /*----------------------------------------------------------------------------*/
 /* selfg_by_fft_2d:
  *   Only works for uniform grid, periodic boundary conditions, and dx1=dx2
@@ -121,13 +128,6 @@ void selfg_by_fft_2d(Grid *pG, Domain *pD)
   work[F2DI(0,0,pG->Nx1,pG->Nx2)][0] = 0.0;
   work[F2DI(0,0,pG->Nx1,pG->Nx2)][1] = 0.0;
 
-  for (i=is+1; i<=ie; i++){
-    work[F2DI(i-is,0,pG->Nx1,pG->Nx2)][0] *= SQR(pG->dx1)/
-     (2.0*cos((i-is)*dkx) - 2.0);
-    work[F2DI(i-is,0,pG->Nx1,pG->Nx2)][1] *= SQR(pG->dx1)/
-     (2.0*cos((i-is)*dkx) - 2.0);
-  }
-
   for (j=js+1; j<=je; j++){
     work[F2DI(0,j-js,pG->Nx1,pG->Nx2)][0] *= SQR(pG->dx1)/
      (2.0*cos((j-js)*dky) - 2.0);
@@ -136,7 +136,7 @@ void selfg_by_fft_2d(Grid *pG, Domain *pD)
   }
 
   for (i=is+1; i<=ie; i++){
-    for (j=js+1; j<=je; j++){
+    for (j=js; j<=je; j++){
       work[F2DI(i-is,j-js,pG->Nx1,pG->Nx2)][0] *= SQR(pG->dx1)/
        (2.0*cos((i-is)*dkx) + 2.0*cos((j-js)*dky) - 4.0);
       work[F2DI(i-is,j-js,pG->Nx1,pG->Nx2)][1] *= SQR(pG->dx1)/
@@ -177,7 +177,7 @@ void selfg_by_fft_2d(Grid *pG, Domain *pD)
 
 /*----------------------------------------------------------------------------*/
 /* selfg_by_fft_3d:
- *   Only works for uniform grid, periodic boundary conditions 
+ *   Only works for uniform grid, periodic boundary conditions, and dx1=dx2=dx3
  */
 
 void selfg_by_fft_3d(Grid *pG, Domain *pD)
@@ -186,6 +186,7 @@ void selfg_by_fft_3d(Grid *pG, Domain *pD)
   int i, is = pG->is, ie = pG->ie;
   int j, js = pG->js, je = pG->je;
   int k, ks = pG->ks, ke = pG->ke;
+  Real dkx,dky,dkz;
 
 /* Copy current potential into old */
 
@@ -196,6 +197,8 @@ void selfg_by_fft_3d(Grid *pG, Domain *pD)
     }
   }}
 
+/* Forward FFT of 4\piG*(d-d0) */
+
   for (k=ks; k<=ke; k++){
   for (j=js; j<=je; j++){
     for (i=is; i<=ie; i++){
@@ -205,23 +208,45 @@ void selfg_by_fft_3d(Grid *pG, Domain *pD)
     }
   }}
 
-  printf("In 3D fft, %e\n",four_pi_G);
-
   ath_3d_fft(fplan3d, work);
 
-  for (k=ks; k<=ke; k++){
+/* Compute potential in Fourier space.  Multiple loops are used to avoid divide
+ * by zero at i=is,j=js   */
+
+  dkx = 2.0*PI/(double)(pD->ixe - pD->ixs + 1);
+  dky = 2.0*PI/(double)(pD->jxe - pD->jxs + 1);
+  dkz = 2.0*PI/(double)(pD->kxe - pD->kxs + 1);
+
+  work[F3DI(0,0,0,pG->Nx1,pG->Nx2,pG->Nx3)][0] = 0.0;
+  work[F3DI(0,0,0,pG->Nx1,pG->Nx2,pG->Nx3)][1] = 0.0;
+
+  for (k=ks+1; k<=ke; k++){
+    work[F3DI(0,0,k-ks,pG->Nx1,pG->Nx2,pG->Nx3)][0] *= SQR(pG->dx1)/
+     (2.0*cos((k-ks)*dkz) - 2.0);
+    work[F3DI(0,0,k-ks,pG->Nx1,pG->Nx2,pG->Nx3)][1] *= SQR(pG->dx1)/
+     (2.0*cos((k-ks)*dkz) - 2.0);
+  }
+
+  for (j=js+1; j<=je; j++){
+    for (k=ks; k<=ke; k++){
+      work[F3DI(0,j-js,k-ks,pG->Nx1,pG->Nx2,pG->Nx3)][0] *= SQR(pG->dx1)/
+       (2.0*cos((j-js)*dky) + 2.0*cos((k-ks)*dkz) - 4.0);
+      work[F3DI(0,j-js,k-ks,pG->Nx1,pG->Nx2,pG->Nx3)][1] *= SQR(pG->dx1)/
+       (2.0*cos((j-js)*dky) + 2.0*cos((k-ks)*dkz) - 4.0);
+    }
+  }
+
+  for (i=is+1; i<=ie; i++){
   for (j=js; j<=je; j++){
-    for (i=is; i<=ie; i++){
-      work[F3DI(i,j,k,pG->Nx1,pG->Nx2,pG->Nx3)][0] /= 
-       (SQR(KCOMP(i-is, is+pG->idisp, pD->ixe-pD->ixs+1)) +
-        SQR(KCOMP(j-js, js+pG->jdisp, pD->jxe-pD->jxs+1)) +
-        SQR(KCOMP(k-ks, ks+pG->kdisp, pD->kxe-pD->kxs+1)) );
-      work[F3DI(i,j,k,pG->Nx1,pG->Nx2,pG->Nx3)][1] /=
-       (SQR(KCOMP(i-is, is+pG->idisp, pD->ixe-pD->ixs+1)) +
-        SQR(KCOMP(j-js, js+pG->jdisp, pD->jxe-pD->jxs+1)) +
-        SQR(KCOMP(k-ks, ks+pG->kdisp, pD->kxe-pD->kxs+1)) );
+    for (k=ks; k<=ke; k++){
+      work[F3DI(i-is,j-js,k-ks,pG->Nx1,pG->Nx2,pG->Nx3)][0] *= SQR(pG->dx1)/
+       (2.0*cos((i-is)*dkx) + 2.0*cos((j-js)*dky) + 2.0*cos((k-ks)*dkz) - 6.0);
+      work[F3DI(i-is,j-js,k-ks,pG->Nx1,pG->Nx2,pG->Nx3)][1] *= SQR(pG->dx1)/
+       (2.0*cos((i-is)*dkx) + 2.0*cos((j-js)*dky) + 2.0*cos((k-ks)*dkz) - 6.0);
     }
   }}
+
+/* Backward FFT and set potential in real space */
 
   ath_3d_fft(bplan3d, work);
 
@@ -232,6 +257,35 @@ void selfg_by_fft_3d(Grid *pG, Domain *pD)
         bplan3d->cnt;
     }
   }}
+
+/* Set periodic boundary conditions */
+
+  for (k=ks; k<=ke; k++){
+    for (j=js; j<=je; j++){
+      for (i=1; i<=nghost; i++) {
+        pG->Phi[k][j][is-i] = pG->Phi[k][j][ie-(i-1)];
+        pG->Phi[k][j][ie+i] = pG->Phi[k][j][is+(i-1)];
+      }
+    }
+  }
+
+  for (k=ks; k<=ke; k++){
+    for (j=1; j<=nghost; j++) {
+      for (i=is-nghost; i<=ie+nghost; i++) {
+        pG->Phi[k][js-j][i] = pG->Phi[k][je-(j-1)][i];
+        pG->Phi[k][je+j][i] = pG->Phi[k][js+(j-1)][i];
+      }
+    }
+  }
+
+  for (k=1; k<=nghost; k++) {
+    for (j=js-nghost; j<=je+nghost; j++) {
+      for (i=is-nghost; i<=ie+nghost; i++) {
+        pG->Phi[ks-k][j][i] = pG->Phi[ke-(k-1)][j][i];
+        pG->Phi[ke+k][j][i] = pG->Phi[ks+(k-1)][j][i];
+      }
+    }
+  }
 
 #endif /* SELF_GRAVITY_USING_FFT */
   return;
@@ -248,7 +302,6 @@ void selfg_by_fft_2d_init(Grid *pG, Domain *pD)
   fplan2d = ath_2d_fft_quick_plan(pG, pD, NULL, ATH_FFT_FORWARD);
   bplan2d = ath_2d_fft_quick_plan(pG, pD, NULL, ATH_FFT_BACKWARD);
   work = ath_2d_fft_malloc(fplan2d);
-  printf("Initializing 2D fft %ld\n", fplan2d->cnt);
 }
 
 /*----------------------------------------------------------------------------*/
@@ -262,7 +315,6 @@ void selfg_by_fft_3d_init(Grid *pG, Domain *pD)
   fplan3d = ath_3d_fft_quick_plan(pG, pD, NULL, ATH_FFT_FORWARD);
   bplan3d = ath_3d_fft_quick_plan(pG, pD, NULL, ATH_FFT_BACKWARD);
   work = ath_3d_fft_malloc(fplan3d);
-  printf("Initializing 3D fft\n");
 }
 
 #endif /* FFT_ENABLED */
