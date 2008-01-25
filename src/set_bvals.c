da4aa3c733d45aa6b9adc761d038da29dc9a448e
jstone <jstone@aae0e298-1656-4984-9a04-95235c055e13>
2008-01-25 21:50:38 +0000

    Added call to shear_ix1_ox1 for shearing box BCs.  Using standard function pointers doesn't work with MPI, since ix1 and ox2 BCs must be executed at the same time (to send and receive messages).

--- src/set_bvals.c
+++ src/set_bvals.c
@@ -131,6 +131,10 @@ static void receive_ix3(Grid *pG, int var_flag, MPI_Request *prq);
 static void receive_ox3(Grid *pG, int var_flag, MPI_Request *prq);
 #endif /* MPI_PARALLEL */
 
+#ifdef SHEARING_BOX
+void shear_ix1_ox1(Grid *pGrid, int var_flag);
+#endif
+
 /*=========================== PUBLIC FUNCTIONS ===============================*/
 /*----------------------------------------------------------------------------*/
 /* set_bvals:  calls appropriate functions to set ghost zones.  The function
@@ -154,6 +158,12 @@ void set_bvals(Grid *pGrid, int var_flag)
  * Boundary Conditions in x1-direction */
 
   if (pGrid->Nx1 > 1){
+
+/* For shearing box, ix1 and ox1 boundaries have to be executed simultaneously.
+ * This is implemented in following routine, defined in problem generator */
+#ifdef SHEARING_BOX
+    shear_ix1_ox1(pGrid, var_flag);
+#endif
 
 #ifdef MPI_PARALLEL
     cnt2 = pGrid->Nx2 > 1 ? pGrid->Nx2 + 1 : 1;
