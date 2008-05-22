b385e571965a118a1e9cd2fd7ea1dc08379196da
gardiner <gardiner@aae0e298-1656-4984-9a04-95235c055e13>
2008-05-22 04:27:14 +0000

    I renamed the fname_construct() function to ath_fname() so that it more closely matches ath_fopen().  I changed these two functions to take an optional "path" parameter.  I increased the size of the local filename[] array in ath_fopen() to 1024 chars. and added a failover test in to catch the cases where the desired filename is too large, in which case ath_fname() is used to allocate and construct the filename.  I added a more conservative estimate of the size of the "dump number" field since it can be larger than the desired "dlen" parameter.

--- src/ath_files.c
+++ src/ath_files.c
@@ -4,8 +4,9 @@
  *
  * PURPOSE: Functions for creating descriptive output filenames, and for
  *   opening files with such filenames.  Filename has form:
- *        <basename>[.idump][.id].<ext>
- *   where basename     basename of file (usually problem name, e.g. "Sod")
+ *        [path]<basename>[.idump][.id].<ext>
+ *   where path         optional path
+ *         basename     basename of file (usually problem name, e.g. "Sod")
  *         dlen         number of digits to use for numeric extension (1..10)
  *         idump        optional dump number (0,1,2,.....)
  *                      if(dlen > 0 and idump >= 0) we use the dump number
@@ -14,7 +15,7 @@
  *         ext          file extension, e.g. ".tab", ".bin", ".dx", ".vtk"
  *
  * CONTAINS PUBLIC FUNCTIONS: 
- *   fname_construct()
+ *   ath_fname()
  *   ath_fopen()
  *============================================================================*/
 
@@ -24,41 +25,62 @@
 #include "prototypes.h"
 
 /*----------------------------------------------------------------------------*/
-/* fname_construct: creates filename with form <basename>[.idump][.id].<ext>
+/* ath_fname: creates filename with form [path]<basename>[.idump][.id].<ext>
  *   Used by most of the dump_*() and output_*() functions.
  */
 
-char *fname_construct(const char *basename, const int dlen, const int idump, 
-                      const char *id, const char *ext)
+char *ath_fname(const char *path, const char *basename,
+                const int dlen, const int idump, 
+                const char *id, const char *ext)
 {
   char fmt[80];
-  size_t size;
-  char *fname;
+  size_t size, pathlen = 0;
+  char *cp, *fname;
 
-/* 2 = "." separating basename and extension + NUL terminator */
+/* 2 = "." following the basename + NUL terminator */
   size = 2 + strlen(basename) + strlen(ext);
-  if(dlen > 0) size += 1 + dlen; /* additional 1 for the "." */
-  if(id != NULL) size += 1 + strlen(id); /* additional 1 for the "." */
+  if(path != NULL){
+    pathlen = strlen(path);
+    size += 1 + pathlen; /* add 1 for possible '/' */
+  }
+  if(dlen > 0)
+    size += (dlen > 10 ? dlen : 10) + 1; /* add 1 for the "." */
+  if(id != NULL) size += 1 + strlen(id); /* add 1 for the "." */
 
   if((fname = (char*)malloc(size*sizeof(char))) == NULL){
-    ath_perr(-1,"[fname_construct]: malloc returned a NULL pointer\n");
+    ath_perr(-1,"[ath_fname]: malloc returned a NULL pointer\n");
     return NULL;
   }
 
 /* Build the filename */
+  cp = fname;
+  /* Start with the optional path */
+  if(path != NULL && pathlen > 0){
+    strcpy(cp, path);
+    cp = &(cp[pathlen]); /* point cp at the '\0' terminator */
+
+    /* Append a '/' if necessary */
+    if(cp[-1] != '/'){
+      cp[0] = '/';
+      cp[1] = '\0';
+      cp = &(cp[1]);
+    }
+  }
+
+/* Append the name of the file */
   if(id){
     if(dlen > 0 && idump >= 0){
       sprintf(fmt,"%%s.%%0%dd.%%s.%%s",dlen);
-      sprintf(fname,fmt,basename,idump,id,ext);
+      sprintf(cp,fmt,basename,idump,id,ext);
     }
-    else sprintf(fname,"%s.%s.%s",basename,id,ext);
+    else sprintf(cp,"%s.%s.%s",basename,id,ext);
   }
   else{
     if(dlen > 0 && idump >= 0){
       sprintf(fmt,"%%s.%%0%dd.%%s",dlen);
-      sprintf(fname,fmt,basename,idump,ext);
+      sprintf(cp,fmt,basename,idump,ext);
     }
-    else sprintf(fname,"%s.%s",basename,ext);
+    else sprintf(cp,"%s.%s",basename,ext);
   }
 
   return fname;
@@ -72,27 +94,72 @@ char *fname_construct(const char *basename, const int dlen, const int idump,
  *   Used, for example, for "hst", "tab", "vtk", "pdf", and "rst" dumps/outputs
  */
 
-FILE *ath_fopen(const char *basename, const int dlen, const int idump, 
+FILE *ath_fopen(const char *path, const char *basename,
+                const int dlen, const int idump, 
                 const char *id, const char *ext, const char *mode)
 {
   char fmt[80];
-  char filename[256];
+  size_t size, pathlen = 0;
+  char *cp, filename[1024];
   FILE *fp;
 
+  /* 2 = "." following the basename + NUL terminator */
+  size = 2 + strlen(basename) + strlen(ext);
+  if(path != NULL){
+    pathlen = strlen(path);
+    size += 1 + pathlen; /* add 1 for possible '/' */
+  }
+  if(dlen > 0)
+    size += (dlen > 10 ? dlen : 10) + 1; /* add 1 for the "." */
+  if(id != NULL) size += 1 + strlen(id); /* add 1 for the "." */
+
+  /* If the file name is too large, allocate space for it... */
+  if(size > 1024){
+    char *fname;
+
+    ath_perr(0,"[sim_fopen]: Warning filename length %zu > 1024\n",size);
+
+    fname = ath_fname(path, basename, dlen, idump, id, ext);
+    if(fname == NULL){
+      ath_perr(-1,"[sim_fopen]: Error allocating a filename\n");
+      return NULL;
+    }
+
+    fp = fopen(fname, mode);
+    free(fname);
+
+    return fp;
+  }
+
 /* Build the filename */
+  cp = filename;
+  /* Start with the optional path */
+  if(path != NULL && pathlen > 0){
+    strcpy(cp, path);
+    cp = &(cp[pathlen]); /* point cp at the '\0' terminator */
+
+    /* Append a '/' if necessary */
+    if(cp[-1] != '/'){
+      cp[0] = '/';
+      cp[1] = '\0';
+      cp = &(cp[1]);
+    }
+  }
+
+/* Append the name of the file */
   if(id){
     if(dlen > 0 && idump >= 0){
       sprintf(fmt,"%%s.%%0%dd.%%s.%%s",dlen);
-      sprintf(filename,fmt,basename,idump,id,ext);
+      sprintf(cp,fmt,basename,idump,id,ext);
     }
-    else sprintf(filename,"%s.%s.%s",basename,id,ext);
+    else sprintf(cp,"%s.%s.%s",basename,id,ext);
   }
   else{
     if(dlen > 0 && idump >= 0){
       sprintf(fmt,"%%s.%%0%dd.%%s",dlen);
-      sprintf(filename,fmt,basename,idump,ext);
+      sprintf(cp,fmt,basename,idump,ext);
     }
-    else sprintf(filename,"%s.%s",basename,ext);
+    else sprintf(cp,"%s.%s",basename,ext);
   }
 
   fp = fopen(filename,mode);
