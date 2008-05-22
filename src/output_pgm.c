8290dd892a94285a8aa04da0b6e321f66e0d8286
gardiner <gardiner@aae0e298-1656-4984-9a04-95235c055e13>
2008-05-22 04:36:20 +0000

    I updated the fname_construct() call to ath_fname() and added the optional path argument, set to NULL, for ath_fname().

--- src/output_pgm.c
+++ src/output_pgm.c
@@ -38,7 +38,7 @@ void output_pgm(Grid *pGrid, Domain *pD, Output *pOut)
 /* construct output filename.  pOut->id will either be name of variable,
  * if 'id=...' was included in <ouput> block, or 'outN' where N is number of
  * <output> block.  */
-  if((fname = fname_construct(pGrid->outfilename,num_digit,dnum,pOut->id,"pgm"))
+  if((fname = ath_fname(NULL,pGrid->outfilename,num_digit,dnum,pOut->id,"pgm"))
      == NULL){
     ath_error("[output_pgm]: Error constructing filename\n");
     return;
