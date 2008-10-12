056d7779e1911fb1f629bf58eb87cc5bd75994ea
jmstone <jmstone@aae0e298-1656-4984-9a04-95235c055e13>
2008-10-12 22:04:53 +0000

    Problem generator for cooling instability runs.  Right now very simple, for testing only.  Will expand in future to run Piontek & Ostriker, etc., type problems.

---
+++ src/prob/ti.c
@@ -0,0 +1,183 @@
+#include "copyright.h"
+/*==============================================================================
+ * FILE: ti.c
+ *
+ * PURPOSE: Problem generator for thermal instability.  Setup to use the 
+ *   Koyama & Inutsuka cooling function, so must use cgs units for calculations
+ *============================================================================*/
+
+#include <float.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "defs.h"
+#include "athena.h"
+#include "globals.h"
+#include "prototypes.h"
+
+/* These constants are in cgs */
+static const Real mbar = (1.37)*(1.6733e-24);
+static const Real kb = 1.380658e-16;
+
+/*==============================================================================
+ * PRIVATE FUNCTION PROTOTYPES:
+ * ran2()
+ *============================================================================*/
+
+static double ran2(long int *idum);
+
+/*=========================== PUBLIC FUNCTIONS ===============================*/
+/*----------------------------------------------------------------------------*/
+/* problem:  */
+
+void problem(Grid *pGrid, Domain *pDomain)
+{
+  int i=0,j=0,k=0;
+  int is,ie,js,je,ks,ke;
+  Real n0,T0;
+  long int iseed = -1;
+
+  is = pGrid->is; ie = pGrid->ie;
+  js = pGrid->js; je = pGrid->je;
+  ks = pGrid->ks; ke = pGrid->ke;
+
+/* Read problem parameters */
+
+  T0    = par_getd("problem","T0");
+  n0    = par_getd("problem","n0");
+
+/* Constant density and temperature initially */
+
+  for (k=ks; k<=ke; k++) {
+  for (j=js; j<=je; j++) {
+    for (i=is; i<=ie; i++) {
+      pGrid->U[k][j][i].d = n0*mbar;
+      pGrid->U[k][j][i].M1 = 0.0;
+      pGrid->U[k][j][i].M2 = 0.0;
+      pGrid->U[k][j][i].M3 = 0.0;
+/* Pressure scaled to give a sound speed of 1 with gamma=1.4 */
+      pGrid->U[k][j][i].E = (n0*kb*T0)/Gamma_1
+             + 0.5*(SQR(pGrid->U[k][j][i].M1) + SQR(pGrid->U[k][j][i].M2)
+             + SQR(pGrid->U[k][j][i].M3))/pGrid->U[k][j][i].d;
+    }
+  }}
+
+  CoolingFunc = KoyInut;
+
+}
+
+/*==============================================================================
+ * PROBLEM USER FUNCTIONS:
+ * problem_write_restart() - writes problem-specific user data to restart files
+ * problem_read_restart()  - reads problem-specific user data from restart files
+ * get_usr_expr()          - sets pointer to expression for special output data
+ * get_usr_out_fun()       - returns a user defined output function pointer
+ * Userwork_in_loop        - problem specific work IN     main loop
+ * Userwork_after_loop     - problem specific work AFTER  main loop
+ *----------------------------------------------------------------------------*/
+
+void problem_write_restart(Grid *pG, Domain *pD, FILE *fp)
+{
+  return;
+}
+
+void problem_read_restart(Grid *pG, Domain *pD, FILE *fp)
+{
+  return;
+}
+
+Gasfun_t get_usr_expr(const char *expr)
+{
+  return NULL;
+}
+
+VGFunout_t get_usr_out_fun(const char *name){
+  return NULL;
+}
+
+void Userwork_in_loop(Grid *pGrid, Domain *pDomain)
+{
+  return;
+}
+
+void Userwork_after_loop(Grid *pGrid, Domain *pDomain)
+{
+  return;
+}
+
+/*=========================== PRIVATE FUNCTIONS ==============================*/
+/*------------------------------------------------------------------------------
+ * ran2: extracted from the Numerical Recipes in C (version 2) code.  Modified
+ *   to use doubles instead of floats. -- T. A. Gardiner -- Aug. 12, 2003
+ */
+
+#define IM1 2147483563
+#define IM2 2147483399
+#define AM (1.0/IM1)
+#define IMM1 (IM1-1)
+#define IA1 40014
+#define IA2 40692
+#define IQ1 53668
+#define IQ2 52774
+#define IR1 12211
+#define IR2 3791
+#define NTAB 32
+#define NDIV (1+IMM1/NTAB)
+#define RNMX (1.0-DBL_EPSILON)
+
+/* Long period (> 2 x 10^{18}) random number generator of L'Ecuyer
+ * with Bays-Durham shuffle and added safeguards.  Returns a uniform
+ * random deviate between 0.0 and 1.0 (exclusive of the endpoint
+ * values).  Call with idum = a negative integer to initialize;
+ * thereafter, do not alter idum between successive deviates in a
+ * sequence.  RNMX should appriximate the largest floating point value
+ * that is less than 1. 
+
+ */
+double ran2(long int *idum){
+  int j;
+  long int k;
+  static long int idum2=123456789;
+  static long int iy=0;
+  static long int iv[NTAB];
+  double temp;
+
+  if (*idum <= 0) { /* Initialize */
+    if (-(*idum) < 1) *idum=1; /* Be sure to prevent idum = 0 */
+    else *idum = -(*idum);
+    idum2=(*idum);
+    for (j=NTAB+7;j>=0;j--) { /* Load the shuffle table (after 8 warm-ups) */
+      k=(*idum)/IQ1;
+      *idum=IA1*(*idum-k*IQ1)-k*IR1;
+      if (*idum < 0) *idum += IM1;
+      if (j < NTAB) iv[j] = *idum;
+    }
+    iy=iv[0];
+  }
+  k=(*idum)/IQ1;                 /* Start here when not initializing */
+  *idum=IA1*(*idum-k*IQ1)-k*IR1; /* Compute idum=(IA1*idum) % IM1 without */
+  if (*idum < 0) *idum += IM1;   /* overflows by Schrage's method */
+  k=idum2/IQ2;
+  idum2=IA2*(idum2-k*IQ2)-k*IR2; /* Compute idum2=(IA2*idum) % IM2 likewise */
+  if (idum2 < 0) idum2 += IM2;
+  j=(int)(iy/NDIV);              /* Will be in the range 0...NTAB-1 */
+  iy=iv[j]-idum2;                /* Here idum is shuffled, idum and idum2 */
+  iv[j] = *idum;                 /* are combined to generate output */
+  if (iy < 1) iy += IMM1;
+  if ((temp=AM*iy) > RNMX) return RNMX; /* No endpoint values */
+  else return temp;
+}
+
+#undef IM1
+#undef IM2
+#undef AM
+#undef IMM1
+#undef IA1
+#undef IA2
+#undef IQ1
+#undef IQ2
+#undef IR1
+#undef IR2
+#undef NTAB
+#undef NDIV
+#undef RNMX
