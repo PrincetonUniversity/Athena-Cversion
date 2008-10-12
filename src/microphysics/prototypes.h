a10384d08d5280b0b34a3174dde5d21bc381178e
jmstone <jmstone@aae0e298-1656-4984-9a04-95235c055e13>
2008-10-12 22:00:44 +0000

    (cf ticket #5 #6 #7)  Added optically thin cooling function for ISM in microphysics sub-directory.  Modified makefile and prototypes.  Cooling can now be enrolled using function pointers like static gravity.

--- src/microphysics/prototypes.h
+++ src/microphysics/prototypes.h
@@ -6,6 +6,7 @@
  *
  * PURPOSE: Prototypes for all public functions from the following files:
  *   braginskii.c
+ *   cool.c
  *   resistivity.c
  *   viscosity.c
  *============================================================================*/
@@ -20,6 +21,9 @@
 /* braginskii.c */
 void braginskii_3d(Grid *pG, Domain *pD);
 void braginskii_init_3d(int nx1, int nx2, int nx3);
+
+/* cool.c */
+Real KoyInut(const Real dens, const Real Press, const Real dt);
 
 /* resistivity.c */
 void resistivity_3d(Grid *pG, Domain *pD);
