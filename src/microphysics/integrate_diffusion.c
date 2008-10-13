4366948ae3f21d6ed4b27298134951c9a7860827
jmstone <jmstone@aae0e298-1656-4984-9a04-95235c055e13>
2008-10-13 19:30:08 +0000

    Added new function to integrate diffusive source terms, called by main loop. It also calls special init and destruct functions to allocate memory needed by diffusion routines.  It also sets timestep for diffusion terms.  Also added changes to Makefile and prototypes for this (and other) new functions.

---
+++ src/microphysics/integrate_diffusion.c
@@ -0,0 +1,171 @@
+#include "../copyright.h"
+/*==============================================================================
+ * FILE: integrate_diffusion.c
+ *
+ * PURPOSE: Contains public functions to integrate explicit diffusion terms
+ *   using operator splitting.
+ *
+ * CONTAINS PUBLIC FUNCTIONS: 
+ *   integrate_explicit_diff() - calls functions for each diffusion operator
+ *   integrate_explicit_diff_init() - allocates memory for diff functions
+ *   integrate_explicit_diff_destruct() - frees memory for diff functions
+ *============================================================================*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../defs.h"
+#include "../athena.h"
+#include "../globals.h"
+#include "../prototypes.h"
+#include "prototypes.h"
+
+/* function pointers for diffusion operators (determined at runtime) */
+static VGDFun_t ApplyViscosity=NULL;
+static VGDFun_t ApplyResistivity=NULL;
+static VGDFun_t ApplyThermalConduct=NULL;
+
+/* dimension of calculation (determined at runtime) */
+static int dim=0;
+
+/*----------------------------------------------------------------------------*/
+/* integrate_explicit_diff:  called in main loop, sets timestep and calls
+ *   appropriate functions for each diffusion operator
+ */
+
+void integrate_explicit_diff(Grid *pGrid, Domain *pDomain)
+{
+  Real min_dx,min_dt=0.0;
+
+/* Calculate explicit diffusion timestep */
+
+  min_dx = MIN(pGrid->dx1,(MIN(pGrid->dx2,pGrid->dx3)));
+#ifdef OHMIC
+  pGrid->dt = MIN(pGrid->dt,((min_dx*min_dx)/(4.0*eta_R)));
+#endif        
+#if defined(NAVIER_STOKES) || defined(BRAGINSKII)
+  pGrid->dt = MIN(pGrid->dt,((min_dx*min_dx)/(4.0*nu_V)));
+#endif
+#if defined(ISOTROPIC_CONDUCTION) || defined(ANISOTROPIC_CONDUCTION)
+  pGrid->dt = MIN(pGrid->dt,((min_dx*min_dx)/(4.0*kappa_T)));
+#endif
+
+/* Call diffusion operators.  Function pointers set in 
+ * integrate_explicit_diff_init() */
+
+  if (ApplyViscosity != NULL) (*ApplyViscosity)(pGrid, pDomain);
+
+  if (ApplyResistivity != NULL) (*ApplyResistivity)(pGrid, pDomain);
+
+  if (ApplyThermalConduct != NULL) (*ApplyThermalConduct)(pGrid, pDomain);
+
+  return;
+}
+
+
+/*----------------------------------------------------------------------------*/
+/* integrate_explicit_diff_init: Set function pointers for diffusion 
+ *   operators, call initialization routines to allocate memory
+ */
+
+void integrate_explicit_diff_init(Grid *pGrid, Domain *pDomain)
+{   
+/* Calculate the dimensions  */
+  dim=0;
+  if(pGrid->Nx1 > 1) dim++;
+  if(pGrid->Nx2 > 1) dim++;
+  if(pGrid->Nx3 > 1) dim++;
+
+/* Set function pointers for viscosity, resistivity, and anisotropic conduction
+ * based on dimension of problem, and macros set by configure 
+ */
+
+  switch(dim){
+
+/* 1D (braginskii viscosity and anisotropic conduction not allowed) */
+  case 1:
+#ifdef NAVIER_STOKES
+    ApplyViscosity = ns_viscosity_1d;
+    ns_viscosity_init(pGrid->Nx1, pGrid->Nx2, pGrid->Nx3);
+#endif
+#ifdef OHMIC
+    ApplyResistivity = ohmic_resistivity_1d;
+    ohmic_resistivity_init(pGrid->Nx1, pGrid->Nx2, pGrid->Nx3);
+#endif
+    break;
+
+/* 2D */
+  case 2:
+#ifdef NAVIER_STOKES
+    ApplyViscosity = ns_viscosity_2d;
+    ns_viscosity_init(pGrid->Nx1, pGrid->Nx2, pGrid->Nx3);
+#endif
+#ifdef BRAGINSKII
+    ApplyViscosity = brag_viscosity_2d;
+    brag_viscosity_init(pGrid->Nx1, pGrid->Nx2, pGrid->Nx3);
+#endif
+#ifdef OHMIC
+    ApplyResistivity = ohmic_resistivity_2d;
+    ohmic_resistivity_init(pGrid->Nx1, pGrid->Nx2, pGrid->Nx3);
+#endif
+#ifdef ANISOTROPIC_CONDUCTION
+    ApplyThermalConduct = anisoconduct_2d;
+    anisoconduct_init(pGrid->Nx1, pGrid->Nx2, pGrid->Nx3);
+#endif
+    break;
+
+/* 3D */
+  case 3:
+#ifdef NAVIER_STOKES
+    ApplyViscosity = ns_viscosity_3d;
+    ns_viscosity_init(pGrid->Nx1, pGrid->Nx2, pGrid->Nx3);
+#endif
+#ifdef BRAGINSKII
+    ApplyViscosity = brag_viscosity_3d;
+    brag_viscosity_init(pGrid->Nx1, pGrid->Nx2, pGrid->Nx3);
+#endif
+#ifdef OHMIC
+    ApplyResistivity = ohmic_resistivity_3d;
+    ohmic_resistivity_init(pGrid->Nx1, pGrid->Nx2, pGrid->Nx3);
+#endif
+#ifdef ANISOTROPIC_CONDUCTION
+    ApplyThermalConduct = anisoconduct_3d;
+    anisoconduct_init(pGrid->Nx1, pGrid->Nx2, pGrid->Nx3);
+#endif
+    break;
+  }
+
+/* Set function pointer for isotropic thermal diffusion operator (the same
+ * function handles 1d/2d/3d)
+ */
+
+#ifdef ISOTROPIC_CONDUCTION
+  ApplyThermalConduct = isoconduct;
+  isoconduct_init(pGrid->Nx1, pGrid->Nx2, pGrid->Nx3);
+#endif
+
+  return;
+}
+
+
+/*----------------------------------------------------------------------------*/
+/* integrate_destruct:  Frees memory associated with diffusion funcs  */
+
+void integrate_explicit_diff_destruct()
+{
+
+#ifdef NAVIER_STOKES
+  ns_viscosity_destruct();
+#endif
+#ifdef BRAGINSKII
+  brag_viscosity_destruct();
+#endif
+#ifdef OHMIC
+  ohmic_resistivity_destruct();
+#endif
+#ifdef ISOTROPIC_CONDUCTION
+  isoconduct_destruct();
+#endif
+#ifdef ANISOTROPIC_CONDUCTION
+  anisoconduct_destruct();
+#endif
+}
--- src/microphysics/prototypes.h
+++ src/microphysics/prototypes.h
@@ -7,6 +7,8 @@
  * PURPOSE: Prototypes for all public functions from the following files:
  *   braginskii.c
  *   cool.c
+ *   integrate_diffusion.c
+ *   isotropic_conduction.c
  *   resistivity.c
  *   viscosity.c
  *============================================================================*/
@@ -19,17 +21,36 @@
 #include "../config.h"
 
 /* braginskii.c */
-void braginskii_3d(Grid *pG, Domain *pD);
-void braginskii_init_3d(int nx1, int nx2, int nx3);
+void brag_viscosity_2d(Grid *pG, Domain *pD);
+void brag_viscosity_3d(Grid *pG, Domain *pD);
+void brag_viscosity_init(int nx1, int nx2, int nx3);
+void brag_viscosity_destruct(void);
 
 /* cool.c */
 Real KoyInut(const Real dens, const Real Press, const Real dt);
 
+/* integrate_diffusion.c */
+void integrate_explicit_diff(Grid *pGrid, Domain *pDomain);
+void integrate_explicit_diff_init(Grid *pGrid, Domain *pDomain);
+void integrate_explicit_diff_destruct(void);
+
+/* isotropic_conduction.c */
+void isoconduct(Grid *pG, Domain *pD);
+void isoconduct_init(int nx1, int nx2, int nx3);
+void isoconduct_destruct(void);
+
 /* resistivity.c */
-void resistivity_3d(Grid *pG, Domain *pD);
+void ohmic_resistivity_1d(Grid *pG, Domain *pD);
+void ohmic_resistivity_2d(Grid *pG, Domain *pD);
+void ohmic_resistivity_3d(Grid *pG, Domain *pD);
+void ohmic_resistivity_init(int nx1, int nx2, int nx3);
+void ohmic_resistivity_destruct(void);
 
 /* viscosity.c */
-void viscosity_3d(Grid *pG, Domain *pD);
-void viscosity_init_3d(int nx1, int nx2, int nx3);
+void ns_viscosity_1d(Grid *pG, Domain *pD);
+void ns_viscosity_2d(Grid *pG, Domain *pD);
+void ns_viscosity_3d(Grid *pG, Domain *pD);
+void ns_viscosity_init(int nx1, int nx2, int nx3);
+void ns_viscosity_destruct(void);
 
 #endif /* MICROPHYS_PROTOTYPES_H */
