fb9a52d50ed33e9c2745ae8dec723b33a0928f9d
jmstone <jmstone@aae0e298-1656-4984-9a04-95235c055e13>
2008-12-10 19:07:03 +0000

    Fixed bug which was breaking symmetry in outflow_ix3 routine.  Removed random number generator which was not used and caused confusion.

--- src/prob/bubble.c
+++ src/prob/bubble.c
@@ -17,14 +17,12 @@
 
 /*==============================================================================
  * PRIVATE FUNCTION PROTOTYPES:
- * ran2() - random number generator from NR
  * grav_pot() - gravitational potential for 2D problem (accn in Y)
  * outflow_ix1() - sets BCs on L-x1 (left edge) of grid used in 2D
  * outflow_ox1() - sets BCs on R-x1 (right edge) of grid used in 2D
  * outflow_ox2() - sets BCs on R-x2 (right edge) of grid used in 2D
  *============================================================================*/
 
-static double ran2(long int *idum);
 static Real grav_pot(const Real x1, const Real x2, const Real x3);
 static void outflow_ix1(Grid *pGrid);
 static void outflow_ox1(Grid *pGrid);
@@ -40,7 +38,7 @@ void problem(Grid *pGrid, Domain *pDomain)
 {
   int i=0,j=0,k=0;
   int is,ie,js,je,ks,ke;
-  long int iseed = -1;
+  int ifield;									//magentic configuration
   Real x1,x2,x3,r2,rad_bubble=0.25;;
 #ifdef MHD
   Real b0;
@@ -56,15 +54,10 @@ void problem(Grid *pGrid, Domain *pDomain)
   Nx2 = par_geti("grid","Nx2");
   Nx3 = par_geti("grid","Nx3");
 
-/* Ensure a different initial random seed for each process in an MPI calc. */
-  ixs = pGrid->is + pGrid->idisp;
-  jxs = pGrid->js + pGrid->jdisp;
-  kxs = pGrid->ks + pGrid->kdisp;
-  iseed = -1 - (ixs + Nx1*(jxs + Nx2*kxs));
-
 /* Read magnetic field strength */
 #ifdef MHD
-  b0 = par_getd("problem","b0");
+  b0 = par_getd("problem","b0");						//get ifield
+  ifield = par_getd("problem","ifield");
 #endif
 
 /* Initialize atmosphere, and bubble centered at y=0.25 and radius=rad_bubble
@@ -85,15 +78,27 @@ void problem(Grid *pGrid, Domain *pDomain)
         if (r2 < (rad_bubble*rad_bubble)) {
 	  pGrid->U[k][j][i].d = 0.01;
 	}
-#ifdef MHD
-	pGrid->B1i[k][j][i] = b0;
-	pGrid->U[k][j][i].B1c = b0;
-        pGrid->U[k][j][i].E += 0.5*b0*b0;
+#ifdef MHD									//choose ifield
+	if (ifield==1){
+	 pGrid->B1i[k][j][i] = b0;
+	 pGrid->U[k][j][i].B1c = b0;
+         pGrid->U[k][j][i].E += 0.5*b0*b0;
+	 pGrid->B1i[k][j][ie+1] = b0;
+	}
+	if (ifield==2){
+	 pGrid->B2i[k][j][i] = b0;
+	 pGrid->U[k][j][i].B2c = b0;
+         pGrid->U[k][j][i].E += 0.5*b0*b0;
+	 pGrid->B2i[k][je+1][i] = b0;
+	}
+	if (ifield==3){
+	 pGrid->B3i[k][j][i] = b0;
+	 pGrid->U[k][j][i].B3c = b0;
+         pGrid->U[k][j][i].E += 0.5*b0*b0;
+	 pGrid->B3i[ke+1][je][i] = b0;
+	}
 #endif
       }
-#ifdef MHD
-    pGrid->B1i[k][j][ie+1] = b0;
-#endif
     }
   }
 
@@ -171,85 +176,8 @@ void Userwork_after_loop(Grid *pGrid, Domain *pDomain)
 }
 
 /*=========================== PRIVATE FUNCTIONS ==============================*/
-
-/*------------------------------------------------------------------------------
- * ran2: extracted from the Numerical Recipes in C (version 2) code.  Modified
- *   to use doubles instead of floats. -- T. A. Gardiner -- Aug. 12, 2003
- */
-
-#define IM1 2147483563
-#define IM2 2147483399
-#define AM (1.0/IM1)
-#define IMM1 (IM1-1)
-#define IA1 40014
-#define IA2 40692
-#define IQ1 53668
-#define IQ2 52774
-#define IR1 12211
-#define IR2 3791
-#define NTAB 32
-#define NDIV (1+IMM1/NTAB)
-#define RNMX (1.0-DBL_EPSILON)
-
-/* Long period (> 2 x 10^{18}) random number generator of L'Ecuyer
- * with Bays-Durham shuffle and added safeguards.  Returns a uniform
- * random deviate between 0.0 and 1.0 (exclusive of the endpoint
- * values).  Call with idum = a negative integer to initialize;
- * thereafter, do not alter idum between successive deviates in a
- * sequence.  RNMX should appriximate the largest floating point value
- * that is less than 1. 
- */
-
-double ran2(long int *idum)
-{
-  int j;
-  long int k;
-  static long int idum2=123456789;
-  static long int iy=0;
-  static long int iv[NTAB];
-  double temp;
-
-  if (*idum <= 0) { /* Initialize */
-    if (-(*idum) < 1) *idum=1; /* Be sure to prevent idum = 0 */
-    else *idum = -(*idum);
-    idum2=(*idum);
-    for (j=NTAB+7;j>=0;j--) { /* Load the shuffle table (after 8 warm-ups) */
-      k=(*idum)/IQ1;
-      *idum=IA1*(*idum-k*IQ1)-k*IR1;
-      if (*idum < 0) *idum += IM1;
-      if (j < NTAB) iv[j] = *idum;
-    }
-    iy=iv[0];
-  }
-  k=(*idum)/IQ1;                 /* Start here when not initializing */
-  *idum=IA1*(*idum-k*IQ1)-k*IR1; /* Compute idum=(IA1*idum) % IM1 without */
-  if (*idum < 0) *idum += IM1;   /* overflows by Schrage's method */
-  k=idum2/IQ2;
-  idum2=IA2*(idum2-k*IQ2)-k*IR2; /* Compute idum2=(IA2*idum) % IM2 likewise */
-  if (idum2 < 0) idum2 += IM2;
-  j=(int)(iy/NDIV);              /* Will be in the range 0...NTAB-1 */
-  iy=iv[j]-idum2;                /* Here idum is shuffled, idum and idum2 */
-  iv[j] = *idum;                 /* are combined to generate output */
-  if (iy < 1) iy += IMM1;
-  if ((temp=AM*iy) > RNMX) return RNMX; /* No endpoint values */
-  else return temp;
-}
-
-#undef IM1
-#undef IM2
-#undef AM
-#undef IMM1
-#undef IA1
-#undef IA2
-#undef IQ1
-#undef IQ2
-#undef IR1
-#undef IR2
-#undef NTAB
-#undef NDIV
-#undef RNMX
-
 /* Static gravitational potential */
+
 static Real grav_pot(const Real x1, const Real x2, const Real x3)
 {
   Real r2;
@@ -299,10 +227,9 @@ static void outflow_ix1(Grid *pGrid)
   for (k=ks; k<=ke; k++) {
     for (j=js; j<=je; j++) {
       for (i=1; i<=nghost; i++) {
-        pGrid->B1i[k][j][is-i]   = pGrid->B1i[k][j][is+(i-1)];
-        pGrid->U[k][j][is-i].B1c = pGrid->U[k][j][is+(i-1)].B1c;
+        pGrid->B1i[k][j][is-i]   = pGrid->B1i[k][j][is];
+        pGrid->U[k][j][is-i].B1c = pGrid->U[k][j][is].B1c;
       }
-      if (ibc_x1 == 1) pGrid->B1i[k][j][is] = 0.0;
     }
   }
 
@@ -310,8 +237,8 @@ static void outflow_ix1(Grid *pGrid)
   for (k=ks; k<=ke; k++) {
     for (j=js; j<=ju; j++) {
       for (i=1; i<=nghost; i++) {
-        pGrid->B2i[k][j][is-i]   = pGrid->B2i[k][j][is+(i-1)];
-        pGrid->U[k][j][is-i].B2c = pGrid->U[k][j][is+(i-1)].B2c;
+        pGrid->B2i[k][j][is-i]   = pGrid->B2i[k][j][is];
+        pGrid->U[k][j][is-i].B2c = pGrid->U[k][j][is].B2c;
       }
     }
   }
@@ -320,8 +247,8 @@ static void outflow_ix1(Grid *pGrid)
   for (k=ks; k<=ku; k++) {
     for (j=js; j<=je; j++) {
       for (i=1; i<=nghost; i++) {
-        pGrid->B3i[k][j][is-i]   = pGrid->B3i[k][j][is+(i-1)];
-        pGrid->U[k][j][is-i].B3c = pGrid->U[k][j][is+(i-1)].B3c;
+        pGrid->B3i[k][j][is-i]   = pGrid->B3i[k][j][is];
+        pGrid->U[k][j][is-i].B3c = pGrid->U[k][j][is].B3c;
       }
     }
   }
@@ -372,10 +299,9 @@ static void outflow_ox1(Grid *pGrid)
 /* i=ie+1 is not set for the interface field B1i, except obc_x1=1 */
   for (k=ks; k<=ke; k++) {
     for (j=js; j<=je; j++) {
-      if (obc_x1 == 1 ) pGrid->B1i[k][j][ie+1] = 0.0;
-      for (i=2; i<=nghost; i++) {
-        pGrid->B1i[k][j][ie+i]   = pGrid->B1i[k][j][ie-(i-2)];
-        pGrid->U[k][j][ie+i].B1c = pGrid->U[k][j][ie-(i-2)].B1c;
+      for (i=1; i<=nghost; i++) {
+        if (i>1) pGrid->B1i[k][j][ie+i]   = pGrid->B1i[k][j][ie];
+        pGrid->U[k][j][ie+i].B1c = pGrid->U[k][j][ie].B1c;
       }
     }
   }
@@ -384,8 +310,8 @@ static void outflow_ox1(Grid *pGrid)
   for (k=ks; k<=ke; k++) {
     for (j=js; j<=ju; j++) {
       for (i=1; i<=nghost; i++) {
-        pGrid->B2i[k][j][ie+i]   = pGrid->B2i[k][j][ie-(i-1)];
-        pGrid->U[k][j][ie+i].B2c = pGrid->U[k][j][ie-(i-1)].B2c;
+        pGrid->B2i[k][j][ie+i]   = pGrid->B2i[k][j][ie];
+        pGrid->U[k][j][ie+i].B2c = pGrid->U[k][j][ie].B2c;
       }
     }
   }
@@ -394,8 +320,8 @@ static void outflow_ox1(Grid *pGrid)
   for (k=ks; k<=ku; k++) {
     for (j=js; j<=je; j++) {
       for (i=1; i<=nghost; i++) {
-        pGrid->B3i[k][j][ie+i]   = pGrid->B3i[k][j][ie-(i-1)];
-        pGrid->U[k][j][ie+i].B3c = pGrid->U[k][j][ie-(i-1)].B3c;
+        pGrid->B3i[k][j][ie+i]   = pGrid->B3i[k][j][ie];
+        pGrid->U[k][j][ie+i].B3c = pGrid->U[k][j][ie].B3c;
       }
     }
   }
@@ -453,23 +379,18 @@ static void outflow_ox2(Grid *pGrid)
   for (k=ks; k<=ke; k++) {
     for (j=1; j<=nghost; j++) {
       for (i=il; i<=iu; i++) {
-        pGrid->B1i[k][je+j][i]   = pGrid->B1i[k][je-(j-1)][i];
-        pGrid->U[k][je+j][i].B1c = pGrid->U[k][je-(j-1)][i].B1c;
+        pGrid->B1i[k][je+j][i]   = pGrid->B1i[k][je][i];
+        pGrid->U[k][je+j][i].B1c = pGrid->U[k][je][i].B1c;
       }
     }
   }
 
 /* j=je+1 is not set for the interface field B2i, except obc_x2=1 */
   for (k=ks; k<=ke; k++) {
-    if (obc_x2 == 1) {
+    for (j=1; j<=nghost; j++) {
       for (i=il; i<=iu; i++) {
-        pGrid->B2i[k][je+1][i] = 0.0;
-      }
-    }
-    for (j=2; j<=nghost; j++) {
-      for (i=il; i<=iu; i++) {
-        pGrid->B2i[k][je+j][i]   = pGrid->B2i[k][je-(j-2)][i];
-        pGrid->U[k][je+j][i].B2c = pGrid->U[k][je-(j-2)][i].B2c;
+         if (j>1) pGrid->B2i[k][je+j][i]   = pGrid->B2i[k][je][i];
+        pGrid->U[k][je+j][i].B2c = pGrid->U[k][je][i].B2c;
       }
     }
   }
@@ -478,8 +399,8 @@ static void outflow_ox2(Grid *pGrid)
   for (k=ks; k<=ku; k++) {
     for (j=1; j<=nghost; j++) {
       for (i=il; i<=iu; i++) {
-        pGrid->B3i[k][je+j][i]   = pGrid->B3i[k][je-(j-1)][i];
-        pGrid->U[k][je+j][i].B3c = pGrid->U[k][je-(j-1)][i].B3c;
+        pGrid->B3i[k][je+j][i]   = pGrid->B3i[k][je][i];
+        pGrid->U[k][je+j][i].B3c = pGrid->U[k][je][i].B3c;
       }
     }
   }
@@ -525,7 +446,7 @@ static void outflow_ix3(Grid *pGrid)
              /pGrid->U[ks][j][i].d;
         p0 /= pow((1.0 + r2),-0.75);
 
-        cc_pos(pGrid,i,j,(ks-1),&x1,&x2,&x3);
+        cc_pos(pGrid,i,j,(ks-k),&x1,&x2,&x3);
         r2 = x1*x1 + x2*x2 + x3*x3;
         pGrid->U[ks-k][j][i].d = d0*pow((1.0 + r2),-0.75);
         pGrid->U[ks-k][j][i].M1 = 0.0;
@@ -540,8 +461,8 @@ static void outflow_ix3(Grid *pGrid)
   for (k=1; k<=nghost; k++) {
     for (j=jl; j<=ju; j++) {
       for (i=il; i<=iu; i++) {
-        pGrid->B1i[ks-k][j][i]   = pGrid->B1i[ks+(k-1)][j][i];
-        pGrid->U[ks-k][j][i].B1c = pGrid->U[ks+(k-1)][j][i].B1c;
+        pGrid->B1i[ks-k][j][i]   = pGrid->B1i[ks][j][i];
+        pGrid->U[ks-k][j][i].B1c = pGrid->U[ks][j][i].B1c;
       }
     }
   }
@@ -549,24 +470,17 @@ static void outflow_ix3(Grid *pGrid)
   for (k=1; k<=nghost; k++) {
     for (j=jl; j<=ju; j++) {
       for (i=il; i<=iu; i++) {
-        pGrid->B2i[ks-k][j][i]   = pGrid->B2i[ks+(k-1)][j][i];
-        pGrid->U[ks-k][j][i].B2c = pGrid->U[ks+(k-1)][j][i].B2c;
+        pGrid->B2i[ks-k][j][i]   = pGrid->B2i[ks][j][i];
+        pGrid->U[ks-k][j][i].B2c = pGrid->U[ks][j][i].B2c;
       }
     }
   }
 
-  if (ibc_x3 == 1) {
-    for (j=jl; j<=ju; j++) {
-      for (i=il; i<=iu; i++) {
-        pGrid->B3i[ks][j][i] = 0.0;
-      }
-    }
-  }
   for (k=1; k<=nghost; k++) {
     for (j=jl; j<=ju; j++) {
       for (i=il; i<=iu; i++) {
-        pGrid->B3i[ks-k][j][i]   = pGrid->B3i[ks+(k-1)][j][i];
-        pGrid->U[ks-k][j][i].B3c = pGrid->U[ks+(k-1)][j][i].B3c;
+        pGrid->B3i[ks-k][j][i]   = pGrid->B3i[ks][j][i];
+        pGrid->U[ks-k][j][i].B3c = pGrid->U[ks][j][i].B3c;
       }
     }
   }
@@ -627,8 +541,8 @@ static void outflow_ox3(Grid *pGrid)
   for (k=1; k<=nghost; k++) {
     for (j=jl; j<=ju; j++) {
       for (i=il; i<=iu; i++) {
-        pGrid->B1i[ke+k][j][i]   = pGrid->B1i[ke-(k-1)][j][i];
-        pGrid->U[ke+k][j][i].B1c = pGrid->U[ke-(k-1)][j][i].B1c;
+        pGrid->B1i[ke+k][j][i]   = pGrid->B1i[ke][j][i];
+        pGrid->U[ke+k][j][i].B1c = pGrid->U[ke][j][i].B1c;
       }
     }
   }
@@ -637,25 +551,18 @@ static void outflow_ox3(Grid *pGrid)
   for (k=1; k<=nghost; k++) {
     for (j=jl; j<=ju; j++) {
       for (i=il; i<=iu; i++) {
-        pGrid->B2i[ke+k][j][i]   = pGrid->B2i[ke-(k-1)][j][i];
-        pGrid->U[ke+k][j][i].B2c = pGrid->U[ke-(k-1)][j][i].B2c;
+        pGrid->B2i[ke+k][j][i]   = pGrid->B2i[ke][j][i];
+        pGrid->U[ke+k][j][i].B2c = pGrid->U[ke][j][i].B2c;
       }
     }
   }
 
 /* k=ke+1 is not set for the interface field B3i, except obc_x3=1 */
-  if (obc_x3 == 1) {
+  for (k=1; k<=nghost; k++) {
     for (j=jl; j<=ju; j++) {
       for (i=il; i<=iu; i++) {
-        pGrid->B3i[ke+1][j][i] = 0.0;
-      }
-    }
-  }
-  for (k=2; k<=nghost; k++) {
-    for (j=jl; j<=ju; j++) {
-      for (i=il; i<=iu; i++) {
-        pGrid->B3i[ke+k][j][i]   = pGrid->B3i[ke-(k-2)][j][i];
-        pGrid->U[ke+k][j][i].B3c = pGrid->U[ke-(k-2)][j][i].B3c;
+        if (k>1) pGrid->B3i[ke+k][j][i]   = pGrid->B3i[ke][j][i];
+        pGrid->U[ke+k][j][i].B3c = pGrid->U[ke][j][i].B3c;
       }
     }
   }
