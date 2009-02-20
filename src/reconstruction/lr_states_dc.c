65490169ca18fb1378b1aca69c86bf18b3446bcb
jmstone <jmstone@aae0e298-1656-4984-9a04-95235c055e13>
2009-02-20 00:38:09 +0000

    Moved all reconstruction files to new 'reconstruction' directory.

---
+++ src/reconstruction/lr_states_dc.c
@@ -0,0 +1,70 @@
+#include "copyright.h"
+/*==============================================================================
+ * FILE: lr_states_dc.c
+ *
+ * PURPOSE: First order (donor cell, piecewise constant) spatial reconstruction.
+ *   The L/R-states at the left-interface in each cell are indexed i.
+ *   W_{L,i-1/2} is denoted by Wl[i  ];   W_{R,i-1/2} is denoted by Wr[i  ]
+ *   W_{L,i+1/2} is denoted by Wl[i+1];   W_{R,i+1/2} is denoted by Wr[i+1]
+ *
+ * CONTAINS PUBLIC FUNCTIONS: 
+ *   lr_states()          - computes L/R states
+ *   lr_states_init()     - NoOp function in this case
+ *   lr_states_destruct() - NoOp function in this case
+ *============================================================================*/
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "defs.h"
+#include "athena.h"
+#include "prototypes.h"
+
+#ifdef FIRST_ORDER
+
+/*----------------------------------------------------------------------------*/
+/* lr_states:
+ * Input Arguments:
+ *   W = PRIMITIVE variables at cell centers along 1-D slice
+ *   Bxc = B in direction of slice at cell centers
+ *   dt = timestep;   dtodx = dt/dx
+ *   il,iu = lower and upper indices of zone centers in slice
+ * W must be initialized over [il-1:iu+1]
+ *
+ * Output Arguments:
+ *   Wl,Wr = L/R-states of PRIMITIVE variables at interfaces over [il:iu+1]
+ */
+
+void lr_states(const Prim1D W[], MHDARG( const Real Bxc[] , )
+	       const Real dt, const Real dtodx, const int il, const int iu,
+	       Prim1D Wl[], Prim1D Wr[])
+{
+  int i;
+
+  for (i=il; i<=iu+1; i++) {
+    Wl[i] = W[i-1];
+    Wr[i] = W[i  ];
+  }
+
+  return;
+}
+
+/*----------------------------------------------------------------------------*/
+/* lr_states_init:  NoOp for first order, but included for compatibility
+ *   with integrator (needed for 2nd and 3rd order). */
+
+void lr_states_init(int nx1, int nx2, int nx3)
+{
+  return;
+}
+
+/*----------------------------------------------------------------------------*/
+/* lr_states_destruct:  NoOp for first order, but included for compatibility
+ *   with integrator (needed for 2nd and 3rd order). */
+
+void lr_states_destruct(void)
+{
+  return;
+}
+
+#endif /* FIRST_ORDER */
