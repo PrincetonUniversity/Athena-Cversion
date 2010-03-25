fa782e3b3237e0cd13f4a96619c2aafad5f9024c
jmstone <jmstone@aae0e298-1656-4984-9a04-95235c055e13>
2010-03-25 13:56:48 +0000

    Added Nick Hand's exact Riemann solver for adiabatic hydrodynamics.  It has passed all the 1D and many of the 2D hydro tests (for example, it works well on the Einfeldt strong rarefaction tests).

--- src/rsolvers/exact.c
+++ src/rsolvers/exact.c
@@ -14,6 +14,7 @@
  *
  * HISTORY:
  *   dec-2006  Isothermal hydro version written by Nicole Lemaster
+ *   mar-2010  Adiabatic hydro version written by Nick Hand
  *
  * CONTAINS PUBLIC FUNCTIONS:
  *   fluxes() - all Riemann solvers in Athena must have this function name and
@@ -36,19 +37,17 @@
 #error : The exact flux for MHD has not been implemented.
 #endif /* MHD */
 
-#ifndef ISOTHERMAL
-#error : The exact flux for adiabatic EOS has not been implemented.
-#endif /* ISOTHERMAL */
-
 #if (NSCALARS > 0)
 #error : Passive scalars have not been implemented in the exact flux.
 #endif /* NSCALARS */
 
+#ifdef ISOTHERMAL
+
 static void srder(double dm, double vl, double vr, double dmin, double dmax, 
-			double *y, double *dydx);
+		  double *y, double *dydx);
 static double rtsafe(void (*funcd)(double, double, double, double, double,
-			double *, double *), double x1, double x2, double xacc, 
-			double vl, double vr, double dmin, double dmax);
+				   double *, double *), double x1, double x2, double xacc, 
+		     double vl, double vr, double dmin, double dmax);
 
 /*----------------------------------------------------------------------------*/
 /* fluxes:
@@ -59,8 +58,10 @@ static double rtsafe(void (*funcd)(double, double, double, double, double,
  *     pFlux = pointer to fluxes of CONSERVED variables at cell interface
  */
 
-void fluxes(const Cons1D Ul, const Cons1D Ur,
-            const Prim1D Wl, const Prim1D Wr, Cons1D *pF)
+
+void fluxes(const Cons1DS Ul, const Cons1DS Ur,
+            const Prim1DS Wl, const Prim1DS Wr,
+            const Real Bxi, Cons1DS *pF)
 {
   Real zl, zr, zm, dm, Vxm, Mxm, tmp, dmin, dmax;
   Real sl, sr;    /* Left and right going shock velocity */
@@ -73,15 +74,6 @@ void fluxes(const Cons1D Ul, const Cons1D Ur,
 	      Ul.d, Ur.d);
 
 /*--- Step 1. ------------------------------------------------------------------
- * Convert left- and right- states in conserved to primitive variables.
- */
-
-/*
-  pbl = Cons1D_to_Prim1D(&Ul,&Wl,&Bxi);
-  pbr = Cons1D_to_Prim1D(&Ur,&Wr,&Bxi);
-*/
-
-/*--- Step 2. ------------------------------------------------------------------
  * Compute the density and momentum of the intermediate state
  */
 
@@ -157,7 +149,7 @@ void fluxes(const Cons1D Ul, const Cons1D Ur,
   if (dm < 0.0)
     ath_error("[exact flux]: Solver finds negative density %5.4e\n", dm);
 
-/*--- Step 3. ------------------------------------------------------------------
+/*--- Step 2. ------------------------------------------------------------------
  * Calculate the Interface Flux if the wave speeds are such that we aren't
  * actually in the intermediate state
  */
@@ -241,7 +233,7 @@ void fluxes(const Cons1D Ul, const Cons1D Ur,
 
 /* If we make it this far, then we're in the intermediate state */
 
-/*--- Step 4. ------------------------------------------------------------------
+/*--- Step 3. ------------------------------------------------------------------
  * Calculate the Interface Flux */
 
   if(Vxm >= 0.0){
@@ -328,6 +320,378 @@ static double rtsafe(void (*funcd)(double, double, double, double, double,
 	return rts;
 }
 
-#undef MAXIT
+#undef MAXIT 
+
+#elif defined ADIABATIC
+/*==============================================================================
+ *  ADIABATIC EXACT SOLVER written by Nick Hand
+ *============================================================================*/
+
+/*------------------------------------------------------------------*/
+
+static Real guessP(const Prim1DS Wl, const Prim1DS Wr)
+/* Provides a guess value for the pressure in the center region. 
+   The choice is made according to the Two-Shock approximate
+   Riemman solver algorithm. Returns pressure guess value p_0, or 
+   TOL if p_0 is less than zero */
+{
+ 
+  Real al = sqrt((double) Gamma*Wl.P/Wl.d);   /* left sound speed */
+  Real ar = sqrt((double) Gamma*Wr.P/Wr.d);   /* right sound speed */
+  Real ppv;  /* inital guess for pressure */
+  Real gl, gr, p_0;
+  Real TOL = 1.0e-6; 
+
+  ppv = 0.5*(Wl.P + Wr.P)-0.125*(Wr.Vx-Wl.Vx)*(Wl.d+Wr.d)*(al+ar);
+
+  if (ppv < 0.0)
+    ppv = 0.0;
+
+  gl = sqrt((double) (2.0/(Wl.d*(Gamma+1)))/
+	    ((Gamma-1)*Wl.P/(Gamma+1) + ppv)); 
+  gr = sqrt((double) (2.0/(Wr.d*(Gamma+1)))/
+	    ((Gamma-1)*Wr.P/(Gamma+1) + ppv));
+  p_0 = (gl*Wl.P + gr*Wr.P - (Wr.Vx-Wl.Vx))/(gr + gl); 
+
+  if (p_0 < 0.0)
+    p_0 = TOL; 
+
+  return p_0; 
+}  
+/*------------------------------------------------------------------*/
+
+static Real PFunc(const Prim1DS W, Real POld)
+/* Evaluates the pressure function (see Toro) for the exact 
+   riemann solver, given the pressure POld */
+{
+  Real f; 
+  Real a = sqrt((double) Gamma*W.P/W.d);
+  Real Ak, Bk; 
+
+  /* rarefaction wave */
+  if (POld <= W.P)  
+    f = 2*a/(Gamma-1) * (pow((double) POld/W.P, (double) 
+			     (Gamma-1)/(2*Gamma)) - 1); 
+
+  /* shock wave */
+  else {
+    Ak = 2/(W.d*(Gamma+1)); 
+    Bk = W.P*(Gamma-1)/(Gamma+1); 
+
+    f = (POld - W.P) * sqrt((double) Ak/(POld + Bk)); 
+  }
+
+  return f; 
+}
+   
+/*------------------------------------------------------------------*/
+
+static Real PFuncDeriv(const Prim1DS W, Real POld)
+/* evaluate the derivative of the pressure function (see Toro) 
+   at the pressure value POld */
+{
+  Real fDer; 
+  Real a = sqrt((double) Gamma*W.P/W.d);
+  Real Ak, Bk; 
+
+  /* rarefaction wave */
+  if (POld <= W.P)  
+    fDer = 1/(a * W.d) * pow((double) POld/W.P, (double) -
+			     (Gamma+1)/(2*Gamma)); 
+
+  /* shock wave */
+  else {
+    Ak = 2/(W.d*(Gamma+1)); 
+    Bk = W.P*(Gamma-1)/(Gamma+1); 
+
+    fDer = sqrt((double) Ak/(POld + Bk))
+      *(1.0 - 0.5*(POld - W.P)/(Bk + POld)); 
+  }
+
+  return fDer; 
+}
+   
+/*------------------------------------------------------------------*/ 
+    
+static Real getPC(const Prim1DS Wl, const Prim1DS Wr)
+/* Uses Newton-Raphson iteration to find the alebraic root of the 
+   pressure function and determine the pressure solution in the 
+   center region, Pc. Fails if iteration diverges */
+{
+  Real POld; 
+  Real VxDiff = Wr.Vx - Wl.Vx;
+  int i = 0;   
+  Real TOL = 1.0e-2;
+  Real change, p, fr, fl, frder, flder; 
+  Real MAX_ITER = 100; 
+
+  POld = guessP(Wl, Wr);
+  
+  while (i < MAX_ITER) {
+
+    fl = PFunc(Wl, POld); 
+    fr = PFunc(Wr, POld); 
+    flder = PFuncDeriv(Wl, POld); 
+    frder = PFuncDeriv(Wr, POld); 
+    
+    p = POld - (fl + fr + VxDiff)/(flder + frder);
+
+    change = 2.0*fabs((p-POld)/(p+POld)); 
+    if (change <= TOL)
+      return p;
+    
+    if (p < 0.0)
+      p = TOL; 
+
+    POld = p; 
+    i++; 
+  }
+   
+  /* exit failure due to divergence */
+  ath_error("[exact_flux]: Divergence in Newton-Raphson \
+iteration: p = %e\n", p); 
+  
+}
+/*------------------------------------------------------------------*/
+/* fluxes:
+ *   Input Arguments:
+ *     Bxi = B in direction of slice at cell interface
+ *     Ul,Ur = L/R-states of CONSERVED variables at cell interface
+ *   Output Arguments:
+ *     pF = pointer to fluxes of CONSERVED variables at cell interface
+ */
+
+void fluxes(const Cons1DS Ul, const Cons1DS Ur, 
+	    const Prim1DS Wl, const Prim1DS Wr, 
+	    const Real Bxi, Cons1DS *pF)
+{
+  Real pc, fr, fl; 
+  Real dc, dcl, dcr, Vxc;
+  Real sl, sr;    /* Left and right going shock velocity */
+  Real hdl, hdr;  /* Left and right going rarefaction head velocity */
+  Real tll, tlr;  /* Left and right going rarefaction tail velocity */
+  Real al = sqrt((double) Gamma*Wl.P/Wl.d); /* left sound speed */
+  Real ar = sqrt((double) Gamma*Wr.P/Wr.d); /* right sound speed */
+  Real tmp1, tmp2;
+  Real e, V, E; 
+ 
+  if(!(Ul.d > 0.0)||!(Ur.d > 0.0))
+    ath_error("[exact flux]: Non-positive densities: dl = %e  dr = %e\n", 
+	      Ul.d, Ur.d);
+
+  pc = getPC(Wl, Wr); 
+  
+  /*----------------------------------------------------------------*/
+  /* calculate Vxc */
+  fr = PFunc(Wr, pc); 
+  fl = PFunc(Wl, pc); 
+  Vxc = 0.5*(Wl.Vx + Wr.Vx) + 0.5*(fr - fl); 
+
+  /*-----------------------------------------------------------------*/
+  /* calucate density to left of contact (dcl) */
+  if (pc > Wl.P) {
+
+    /* left shock wave */
+    Real tmp = (Gamma - 1.0) / (Gamma + 1.0);
+    dcl = Wl.d*(pc/Wl.P + tmp)/(tmp*pc/Wl.P + 1); 
+  }
+  else {
+
+    /* left rarefaction wave */
+    dcl = Wl.d*pow((double) (pc/Wl.P), (double) (1/Gamma)); 
+  }
+
+  if (dcl < 0.0)
+     ath_error("[exact flux]: Solver finds negative density %5.4e\n", dcl);
+  
+  /*-----------------------------------------------------------------*/
+  /* calculate density to the right of contact (dcr) */
+  if (pc > Wr.P) {
+
+    /* right shock wave */
+    Real tmp = (Gamma - 1)/(Gamma + 1);
+    dcr = Wr.d*(pc/Wr.P + tmp)/(tmp*pc/Wr.P + 1); 
+  }
+  else {
+
+    /* right rarefaction wave */
+    dcr = Wr.d*pow((double) (pc/Wr.P), (double) (1/Gamma)); 
+  }
+
+  if (dcr < 0.0)
+    ath_error("[exact flux]: Solver finds negative density %5.4e\n", dcr);
+ /*-----------------------------------------------------------------
+  * Calculate the Interface Flux if the wave speeds are such that we aren't
+  * actually in the intermediate state */
+  
+  if (pc > Wl.P) {
+    /* left shock wave */
+    
+    /* left shock speed */
+    sl = Wl.Vx - al*sqrt((double)(pc*(Gamma+1)/(2*Gamma*Wl.P) + 
+				  (Gamma-1)/(2*Gamma))); 
+    if (sl >= 0.0) {
+      /* to left of shock */
+     
+      e = Wl.P/(Wl.d*(Gamma-1));
+      V = Wl.Vx*Wl.Vx + Wl.Vy*Wl.Vy + Wl.Vz*Wl.Vz;
+      E = Wl.d*(0.5*V + e); 
+      
+      pF->E = Wl.Vx*(E + Wl.P);
+      pF->d  = Ul.Mx;
+      pF->Mx = Ul.Mx*(Wl.Vx) + Wl.P;
+      pF->My = Ul.My*(Wl.Vx);
+      pF->Mz = Ul.Mz*(Wl.Vx);
+
+      return; 
+    }
+  }
+  else {
+    /* left rarefaction */
+    
+    Real alc = al*pow((double)(pc/Wl.P), (double)(Gamma-1)/(2*Gamma));
+    
+    hdl = Wl.Vx - al; 
+    tll = Vxc - alc; 
+    
+    if (hdl >= 0.0) {
+      /* To left of rarefaction */
+	
+      e = Wl.P/(Wl.d*(Gamma-1));
+      V = Wl.Vx*Wl.Vx + Wl.Vy*Wl.Vy + Wl.Vz*Wl.Vz;
+      E = Wl.d*(0.5*V + e); 
+      
+      pF->E =  Wl.Vx*(E + Wl.P);
+      pF->d  = Ul.Mx;
+      pF->Mx = Ul.Mx*(Wl.Vx) + Wl.P;
+      pF->My = Ul.My*(Wl.Vx);
+      pF->Mz = Ul.Mz*(Wl.Vx);
+      return;
+      
+    } 
+    else if (tll >= 0.0) {
+      /* Inside rarefaction fan */
+       
+      
+      tmp1 = 2/(Gamma + 1); 
+      tmp2 = (Gamma - 1)/(al*(Gamma+1)); 
+      
+      dc = Wl.d*pow((double)(tmp1 + tmp2*Wl.Vx), (double)(2/(Gamma-1))); 
+      Vxc = tmp1*(al + Wl.Vx*(Gamma-1)/2);
+      pc = Wl.P*pow((double)(tmp1+tmp2*Wl.Vx),(double)(2*Gamma/(Gamma-1))); 
+      
+      e = pc/(dc*(Gamma-1));
+      V = Vxc*Vxc + Wl.Vy*Wl.Vy + Wl.Vz*Wl.Vz;
+      E = dc*(0.5*V + e); 	
+      
+      pF->E = Vxc*(E + pc); 
+      pF->d  = dc*Vxc;
+      pF->Mx = dc*Vxc*Vxc + pc;
+      pF->My = dc*Vxc*Wl.Vy;
+      pF->Mz = dc*Vxc*Wl.Vz;
+      return;
+    } 
+  }
+
+  if (pc > Wr.P) {
+    /* right shock wave */
+    
+    /* right shock speed */
+    sr = Wr.Vx + ar*sqrt((double)(pc*(Gamma+1)/(2*Gamma*Wr.P) + 
+				  (Gamma-1)/(2*Gamma))); 
+    if (sr <= 0.0) {
+      /* to right of shock */
+
+      e = Wr.P/(Wr.d*(Gamma-1));
+      V = Wr.Vx*Wr.Vx + Wr.Vy*Wr.Vy + Wr.Vz*Wr.Vz;
+      E = Wr.d*(0.5*V + e);     
+      
+      pF->E = Wr.Vx*(E + Wr.P);
+      pF->d  = Ur.Mx;
+      pF->Mx = Ur.Mx*(Wr.Vx) + Wr.P;
+      pF->My = Ur.My*(Wr.Vx);
+      pF->Mz = Ur.Mz*(Wr.Vx);
+      return; 
+    }
+  }
+  else {
+    /* right rarefaction */
+    
+    Real arc = ar*pow((double)(pc/Wr.P), (double)(Gamma-1)/(2*Gamma)); 
+      
+    hdr = Wr.Vx + ar; 
+    tlr = Vxc + arc; 
+    
+    if (hdr <= 0.0) {
+      /* To right of rarefaction */
+
+      e = Wr.P/(Wr.d*(Gamma-1));
+      V = Wr.Vx*Wr.Vx + Wr.Vy*Wr.Vy + Wr.Vz*Wr.Vz;
+      E = Wr.d*(0.5*V + e); 	
+      
+      pF->E = Wr.Vx*(E + Wr.P);
+      pF->d  = Ur.Mx;
+      pF->Mx = Ur.Mx*(Wr.Vx) + Wr.P;
+      pF->My = Ur.My*(Wr.Vx);
+      pF->Mz = Ur.Mz*(Wr.Vx);
+      return;
+      
+    } else if (tlr <= 0.0) {
+      /* Inside rarefaction fan */
+      
+      tmp1 = 2/(Gamma + 1); 
+      tmp2 = (Gamma - 1)/(ar*(Gamma+1)); 
+      
+      dc = Wr.d*pow((double)(tmp1 - tmp2*Wr.Vx), (double)(2/(Gamma-1))); 
+      Vxc = tmp1*(-ar + Wr.Vx*(Gamma-1)/2);
+      pc = Wr.P*pow((double)(tmp1-tmp2*Wr.Vx), (double)(2*Gamma/(Gamma-1))); 
+      
+      e = pc/(dc*(Gamma-1));
+      V = Vxc*Vxc + Wr.Vy*Wr.Vy + Wr.Vz*Wr.Vz;
+      E = dc*(0.5*V + e); 
+      
+      pF->E = Vxc*(E + pc);
+      pF->d  = dc*Vxc;
+      pF->Mx = dc*Vxc*Vxc + pc;
+      pF->My = dc*Vxc*Wr.Vy;
+      pF->Mz = dc*Vxc*Wr.Vz;
+      return;
+    }
+  }
+    
+/* We are in the intermediate state */
+
+/*---------------------------------------------------------------------
+ * Calculate the Interface Flux */
+  if (Vxc >= 0.0) {
+
+    e = pc/(dcl*(Gamma-1));
+    V = Vxc*Vxc + Wl.Vy*Wl.Vy + Wl.Vz*Wl.Vz;
+    E = dcl*(0.5*V + e); 
+    
+    pF->E = Vxc*(E + pc); 
+    pF->d  = dcl*Vxc;
+    pF->Mx = dcl*Vxc*Vxc + pc;
+    pF->My = dcl*Vxc*Wl.Vy;
+    pF->Mz = dcl*Vxc*Wl.Vz;
+  }
+  else {
+
+    e = pc/(dcr*(Gamma-1));
+    V = Vxc*Vxc + Wr.Vy*Wr.Vy + Wr.Vz*Wr.Vz;
+    E = dcr*(0.5*V + e); 
+    
+    pF->E = Vxc*(E + pc); 
+    pF->d  = dcr*Vxc;
+    pF->Mx = dcr*Vxc*Vxc + pc;
+    pF->My = dcr*Vxc*Wr.Vy;
+    pF->Mz = dcr*Vxc*Wr.Vz;
+  }
+  
+  return;
+}
+/*------------------------------------------------------------------*/
+
+#endif /*ISOTHERMAL*/
 
 #endif /* EXACT_FLUX */
